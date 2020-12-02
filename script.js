0ce388933dd69559a7549da822a8f1a8e6de432f
Macenna <macennacowen@gwu.edu>
2020-12-01 22:41:39 -0500

    Explanation

--- script.js
+++ script.js
@@ -1,36 +1,20 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-//
 
 console.log("App connecting");
-var submitBtn = document.getElementById("subBtn");
-var passwordText = document.getElementById("password");
+var submitBtn = document.getElementById("subBtn");            // Calling the element ID for the "generate password" button as the value of this variable in order to manipulate it in the DOM
+var passwordText = document.getElementById("password");       // Calling the element ID of the text area so the password var can later be placed in that element 
 
-submitBtn.addEventListener("click", function (event) {
-  event.preventDefault();
-  generatePassword();
+submitBtn.addEventListener("click", function (event) {        // Clicking the submit button is an event that calls the function to begin generating a password 
+  event.preventDefault();                                     // This prevents the submit button from actually submitting a form (the default event for a submit button) in order to generate a password instead 
+  generatePassword();                                         // generatePassword function is declared as the event that is listened for when submit button is clicked 
 });
 
-var numbersString = "1234567890".split("");
-var specialString = "!@#$%^&*()".split("");
+var numbersString = "1234567890".split("");                   // These variables contain strings w/ all the possible characters to randomly call to generate a password
+var specialString = "!@#$%^&*()".split("");                     // .split("") -> separates every character in the string to differentiate each character type for each character category 
 var lowerString = "abcdefghijklmnopqrstuvwxyz".split("");
 var upperString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-var availablePasswordOptions = [];
+var availablePasswordOptions = [];                            // This empty array is where the randomly generated characters will be stored until the functions are finished running (character limit set by user is reached)
 
-console.log(numbersString);
+console.log(numbersString);                                   // The console displays every character as an individual index w/in an array - a different array for each variable 
 console.log(specialString);
 console.log(lowerString);
 console.log(upperString);
